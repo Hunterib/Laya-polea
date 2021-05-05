efea2390297bc4a674fdcc7a674bd797f83ef532
hums <disunit>
2021-05-05 12:40:21 +0800

    添加命令行提示样式

--- src/command.ts
+++ src/command.ts
@@ -1,23 +1,22 @@
-import commander from "commander"
+import commander from "commander";
+import ora from "ora";
 
 export abstract class command {
-    protected program: commander.Command;
-    protected config: any;
-    protected workspace: string;
-    constructor(program?: any) {
-        this.workspace = process.cwd();
-        this.workspace = this.workspace.replace(/\\/g, "/")
-        this.program = program.command(this.constructor.name.toLowerCase())
-        // this.config= require(this.workspace+"/.laya-cli/config");
-        // console.log(config)
-        this.onConstruct();
-        this.program.action((arg) => {
-            this.run(arg);
-        })
-    }
-    /** 初始化命令 */
-    protected abstract onConstruct(): void
+	protected program: commander.Command;
+	protected spinner: ora.Ora = ora({ text: "Loading unicorns", spinner: "boxBounce2" });
+	protected workspace: string;
+	constructor(program?: any) {
+		this.workspace = process.cwd();
+		this.workspace = this.workspace.replace(/\\/g, "/");
+		this.program = program.command(this.constructor.name.toLowerCase());
+		this.onConstruct();
+		this.program.action((arg) => {
+			this.run(arg);
+		});
+	}
+	/** 初始化命令 */
+	protected abstract onConstruct(): void;
 
-    //抽象方法 ，不包含具体实现，要求子类中必须实现此方法
-    protected abstract run(arg?: any): Promise<any>;
+	//抽象方法 ，不包含具体实现，要求子类中必须实现此方法
+	protected abstract run(arg?: any): Promise<any>;
 }
