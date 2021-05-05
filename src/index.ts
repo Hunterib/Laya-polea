680efb54ffaf461c7a956c32cd3bfa649b540193
hums <disunit>
2021-05-05 12:42:28 +0800

    添加获取package.json中的版本号，以及初始化帮助和版本号的命令

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { Command } from 'commander';
-// import { Compile } from './commands/compile';
-// import { publish } from './commands/publish';
-// import { task } from './commands/task';
-var fs = require('fs');
-import path from 'path';
+import { Command } from "commander";
+import fs from "fs";
+import path from "path";
+
+var pjson = require("../package.json");
 
 const program = new Command();
-program.version('0.0.1');
+program.version(pjson.version, "-v, --version", "当前工具的版本号！");
+program.helpOption("-h, --help", "工具的帮助");
+program.addHelpCommand("help [cmd]", "命令的帮助 [cmd]");
 
 var dir = __dirname + "/commands";
 fs.readdirSync(dir).forEach((file: any) => {
-    const pathname = path.join(dir, file)
-    if (!fs.statSync(pathname).isDirectory()) {
-        let cmd = require(pathname);
-        for (const key in cmd) {
-            new cmd[key](program);
-        }
-    }
-})
-
+	const pathname = path.join(dir, file);
+	if (!fs.statSync(pathname).isDirectory()) {
+		let cmd = require(pathname);
+		for (const key in cmd) {
+			new cmd[key](program);
+		}
+	}
+});
 program.parse(process.argv);
