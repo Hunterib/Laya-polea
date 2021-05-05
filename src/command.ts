663cd690832612add6af9972ef92bdc9165a0a41
hums <disunit>
2021-05-05 13:07:59 +0800

    添加命令启动时间戳(纳秒)，修改命令执行方法

--- src/command.ts
+++ src/command.ts
@@ -5,6 +5,7 @@ export abstract class command {
 	protected program: commander.Command;
 	protected spinner: ora.Ora = ora({ text: "Loading unicorns", spinner: "boxBounce2" });
 	protected workspace: string;
+	protected stime: bigint;
 	constructor(program?: any) {
 		this.workspace = process.cwd();
 		this.workspace = this.workspace.replace(/\\/g, "/");
@@ -12,12 +13,13 @@ export abstract class command {
 		this.program.helpOption("-h, --help", "命令帮助");
 		this.onConstruct();
 		this.program.action((arg) => {
-			this.run(arg);
+			this.stime = process.hrtime.bigint();
+			this.execute(arg);
 		});
 	}
 	/** 初始化命令 */
 	protected abstract onConstruct(): void;
 
 	//抽象方法 ，不包含具体实现，要求子类中必须实现此方法
-	protected abstract run(arg?: any): Promise<any>;
+	protected abstract execute(arg?: any): Promise<any>;
 }
