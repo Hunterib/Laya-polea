e1d6cb309ccf5650fea7970f5a818b1d98f6232d
hums <disunit>
2021-05-06 01:32:02 +0800

    添加提示

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,10 @@ var pjson = require("../package.json");
 
 const program = new Command();
 program.version(pjson.version, "-v, --version", chalk.green("当前工具的版本号！"));
-program.helpOption("-h, --help", chalk.green("工具的帮助"));
 program.addHelpCommand("help [cmd]", chalk.green("命令的帮助 [cmd]"));
+program.helpOption("-h, --help", chalk.green("工具的帮助"));
+
+console.log(`\n您正在使用${chalk.cyanBright(" polec ")}编译器 版本: ${chalk.magentaBright(pjson.version)}\n`);
 
 var dir = __dirname + "/commands";
 fs.readdirSync(dir).forEach((file: any) => {
