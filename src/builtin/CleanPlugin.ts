772cb79d3bf982942f35900b4a008f104b859693
Hunterib <82704227+Hunterib@users.noreply.github.com>
2021-05-14 18:49:17 +0800

    Create CleanPlugin.ts
    添加删除文件的插件

---
+++ src/builtin/CleanPlugin.ts
@@ -0,0 +1,20 @@
+import { pluginsCommand } from ".";
+import del from "del";
+
+export class CleanPlugin extends pluginsCommand {
+    protected name: string = "清理目录或者文件";
+
+    /**
+     * 请使用当前目录的相对路径
+     * @param patterns 匹配文件路径
+     * @param force 是否允许删除当前工作目录之外部目录。
+     */
+    constructor(private patterns: string | readonly string[], private force: boolean = false) {
+        super()
+    }
+
+    async execute() {
+        super.execute(arguments);
+        await del(this.patterns, { force: this.force });
+    }
+}
