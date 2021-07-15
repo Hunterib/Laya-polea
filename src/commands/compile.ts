12cdc56752dcd81fd1013fa4a67b594c01adb2a0
hunterib <hunterib@foxmail.com>
2021-07-15 14:16:33 +0800

    Update compile.ts
    添加命令监听的功能

--- src/commands/compile.ts
+++ src/commands/compile.ts
@@ -1,7 +1,8 @@
 import { command } from "../command";
 import { buildConfigEx } from "../tool/config";
 import chalk from "chalk";
-import { ConfigManager} from "../builtin";
+import { ConfigManager, run } from "../builtin";
+import chokidar from "chokidar";
 
 export default class Compile extends command {
     protected onConstruct() {
@@ -14,12 +15,22 @@ export default class Compile extends command {
         this.config = bconf.buildConfig({ command: "compile" });
         if (this.config.plugins && this.config.plugins.length > 0) {
             for (let i = 0; i < this.config.plugins.length; i++) {
+                this.config.plugins[i].UserConfig = this.config;
+                this.config.plugins[i].platform = platform;
+                this.config.plugins[i].command = "compile";
+                this.config.plugins[i].watch = this.config.watch;
                 this.config.plugins[i].workspace = this.workspace;
                 this.config.plugins[i].output = this.config.output;
                 // this.config.plugins[i].spinner = ora({ text: "Loading unicorns", spinner: "boxBounce2" });
                 await this.config.plugins[i].execute();
+                if (this.config.watch) {
+                    this.config.plugins[i].runWatch();
+                }
+                run.Plugin = null;
             }
-            process.exit();
+            if (!this.config.watch) {
+                process.exit();
+            } else {}
         } else {
             process.exit();
         }
